d027ea8b660375443ed1068b5133afa718c8f6dd
Oksana Maiatina <95221256+chickenshrimp@users.noreply.github.com>
2023-12-29 23:31:50 +0300

    edited lab6_task1.c

--- lab6_task1.c
+++ lab6_task1.c
@@ -3,54 +3,53 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 #include <time.h>
 
+
 void printCurrentTime() {
-    time_t now = time(0);
-    struct tm *local_time = localtime(&now);
-    char timeString[40];
-    strftime(timeString, sizeof(timeString), "%H:%M:%S", local_time);
-    
-    printf("%s\n", timeString);
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+  
+    time_t my_time = time(NULL);
+    struct tm *now = localtime(&my_time);
+  
+    printf("%d:%d:%d:%ld\n", now->tm_hour, now->tm_min, now->tm_sec, tv.tv_usec / 1000);
 }
 
 int main() {
-    pid_t child1, child2;
     
     printf("Parent PID: %d\n", getpid());
-    
+    pid_t child1, child2;
     child1 = fork();
-    
-    if (child1 < 0) {
-        printf("Fork failed\n");
-        return 1;
+  
+    if (child1 > 0) {
+        child2 = fork();
+        if (child2 == 0) {
+            printf("Child 2 PID's: %d, Parent's PID's: %d, Current time: ", getpid(), getppid());
+            printCurrentTime();
+            exit(0);
+        
+        }
+      
+        else if (child2 < 0) {
+            printf("Fork failed\n");
+            return 1;
+        }
+        
+        else if (child2 > 0) {
+            system("ps -x | grep 'task1'");
+        }
     }
-    else if (child1 == 0) {
+    
+    else if(child1 == 0){
         printf("Child 1 PID's: %d, Parent's PID's: %d, Current time: ", getpid(), getppid());
         printCurrentTime();
         exit(0);
     }
-    else {
-        child2 = fork();
-        
-        if (child2 < 0) {
-            printf("Fork failed\n");
-            return 1;
-        }
-        else if (child2 == 0) {
-            printf("Child 2 PID's: %d, Parent's PID's: %d, Current time: ", getpid(), getppid());
-            printCurrentTime();
-            exit(0);
-        }
-        else {
-            int status;
-            waitpid(child1, &status, 0);
-            waitpid(child2, &status, 0);
-            
-            printf("Running 'ps -x' using system()\n");
-            system("ps -x");
-        }
+    else{
+        printf("Fork failed\n");
     }
-    
+  
     return 0;
 }
